2d492a439848783e1ae7d510b449116a5993e51a
Meklasmaj <128758763+Meklasmaj@users.noreply.github.com>
2026-02-12 09:24:48 +0100

    Tilføjer første version af CarApp

---
+++ CarApp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CarApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String brand = "";
+            String model = "";
+            int year = 0;
+            char geartype = ' ';
+
+            Console.WriteLine("Instast bilmærke:");
+            brand = Console.ReadLine();
+
+            Console.WriteLine("Instast bilmodel:");
+            model = Console.ReadLine();
+
+            Console.WriteLine("Instast årgang:");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Instast geartype, (A) for automatisk, (M) for manuel:");
+            geartype = Console.ReadLine()[0];
+
+            Console.Clear();
+
+            Console.WriteLine($"{brand} {model} fra {year} har gear: {geartype}");
+            
+            Console.ReadLine();
+        }
+    }
+}
