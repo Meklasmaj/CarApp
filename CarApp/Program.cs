7f7811ca6d42fb5c749034435829307cb7e49f7b
Meklasmaj <meklas@majlandt.dk>
2026-02-19 10:52:41 +0100

    Tilføjer alt til commit

--- CarApp/Program.cs
+++ CarApp/Program.cs
@@ -1,74 +1,61 @@
-﻿namespace CarApp
+﻿using System.Reflection;
+
+namespace CarApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            String brand = "";
-            String model = "";
-            int year = 0;
-            char geartype = ' ';
-            char fueltype = ' ';
-            double kmPrL = 0;
-            int kmNow = 0;
-            double gasPrice = 13.49;
-            double dieselPrice = 12.29;
-            double distance = 0;
-            double tripCost = 0;
-
-            Console.WriteLine("Instast bilmærke:");
-            brand = Console.ReadLine();
-
-            Console.WriteLine("Instast bilmodel:");
-            model = Console.ReadLine();
-
-            Console.WriteLine("Instast årgang:");
-            year = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Instast geartype, (A) for automatisk, (M) for manuel:");
-            geartype = Console.ReadLine()[0];
-
-            Console.WriteLine("Hvilken type brændstof kører bilen på? (B) Benzin / (D) Diesel");
-            fueltype = Console.ReadLine()[0];
-
-            Console.WriteLine("Hvor langt kører bilen på literen?");
-            kmPrL = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Hvor mange kilometer har bilen kørt nu?");
-            kmNow = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Hvor langt skal du køre?");
-            distance = double.Parse(Console.ReadLine());
-
-            if(fueltype == 'B')
-            {
-                tripCost = distance / kmPrL * gasPrice;
-            }
-            else
-            {
-                tripCost = distance / kmPrL * dieselPrice;
-            }
-
-            Console.WriteLine($"Turen på {distance}km, kommer til at koste {tripCost:F2}kr");
-            Console.ReadLine();
-
-            Math.Floor(distance);
-            kmNow += Convert.ToInt32(distance);
+            List<String> brandList = ["Nissan", "Ford", "Ford", "Audi"];
+            List<String> modelList = ["Qashqai", "DMax", "SMax", "A4"];
+            List<int> yearList = [2014, 2015, 2014, 2001];
+            List<int> odometerList = [150000, 166000, 260000, 440000];
+            
 
             // Ending
             Console.Clear();
 
-            string brandLabel = "Bilmærke".PadRight(12);
-            string modelLabel = "Model".PadRight(12);
-            string kmLabel = "Kilometertal";
-            Console.WriteLine($"{brandLabel}|{modelLabel}|{kmLabel}");
-            Console.WriteLine("---------------------------------------");
-            string carInfo = String.Format("{0}|{1}|{2:F0}km", brand.PadRight(12), model.PadRight(12), kmNow);
-            Console.WriteLine(carInfo);
-
-            //Console.WriteLine($"{brand} {model} fra {year} har gear: {geartype}, den kører på {fueltype}, med {kmPrL} km/l og har nu kørt {kmNow}km");
+            
 
             Console.ReadLine();
+
+            void ReadCarDetails()
+            {
+                Console.Clear();
+                Console.WriteLine("Indtast bilmærke:");
+                brandList.Append(Console.ReadLine());
+                Console.WriteLine("Indtast bilmodel:");
+                modelList.Append(Console.ReadLine());
+                Console.WriteLine("Indtast bilens årgang:");
+                yearList.Append(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("Indtast bilens kilometer kørt:");
+                odometerList.Append(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            static double CalculateTripPrice()
+            {
+                //if(fueltype == 'B')
+                //{
+                //    tripCost = distance / kmPrL * gasPrice;
+                //}
+                //else
+                //{
+                //    tripCost = distance / kmPrL * dieselPrice;
+                //}
+                double result = 0;
+                return result;
+            }
+
+            void PrintCarDetails()
+            {
+                string brandLabel = "Bilmærke".PadRight(12);
+                string modelLabel = "Model".PadRight(12);
+                string kmLabel = "Kilometertal";
+                Console.WriteLine($"{brandLabel}|{modelLabel}|{kmLabel}");
+                Console.WriteLine("---------------------------------------");
+                string carInfo = String.Format("{0}|{1}|{2:F0}km", brandList[-1].PadRight(12), modelList[-1].PadRight(12), odometerList[-1]);
+                Console.WriteLine(carInfo);
+            }
         }
     }
 }
