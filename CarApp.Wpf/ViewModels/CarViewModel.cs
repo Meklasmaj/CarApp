cbc388aa9c35404a58754dd3eab78202837f61a9
Meklasmaj <128758763+Meklasmaj@users.noreply.github.com>
2026-05-28 12:23:56 +0200

    Updates changes from Lesson today 28/5

--- CarApp.Wpf/ViewModels/CarViewModel.cs
+++ CarApp.Wpf/ViewModels/CarViewModel.cs
@@ -115,7 +115,7 @@ namespace CarApp.Wpf.ViewModels
                 Cars.Add(car);
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
         protected void OnPropertyChanged(string name) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
