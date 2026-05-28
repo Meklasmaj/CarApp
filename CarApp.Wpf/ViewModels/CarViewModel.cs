1e61f1a21a05ee0577bd8fc6b8ee4648b7b0f2ca
Thomas Schjerlund <thomas@u-man.dk>
2026-05-28 14:10:27 +0200

    Not cloning cars

--- CarApp.Wpf/ViewModels/CarViewModel.cs
+++ CarApp.Wpf/ViewModels/CarViewModel.cs
@@ -21,16 +21,7 @@ namespace CarApp.Wpf.ViewModels
             get => _selectedCar;
             set
             {
-                // Klon bilen så vi ikke redigerer direkte i _cars
-                if (value is FuelCar fc)
-                    _selectedCar = new FuelCar(fc._brand, fc._model, fc._year, fc._licensePlate,
-                        fc._odometer, fc.Usage, fc.Capacity, fc.IsEngineOn, fc.GetFuelType(), fc.Price);
-                else if (value is ElectricCar ec)
-                    _selectedCar = new ElectricCar(ec._brand, ec._model, ec._year, ec._licensePlate,
-                        ec._odometer, ec.Usage, ec.Capacity, ec.IsEngineOn, ec.GetFuelType(), ec.Price);
-                else
-                    _selectedCar = value;
-
+                _selectedCar = value;
                 OnPropertyChanged(nameof(SelectedCar));
                 (UpdateCarCommand as RelayCommand)?.RaiseCanExecuteChanged();
                 (DeleteCarCommand as RelayCommand)?.RaiseCanExecuteChanged();
