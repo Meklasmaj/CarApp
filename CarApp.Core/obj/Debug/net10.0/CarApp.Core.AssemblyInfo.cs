0594165789417ba86ffe16b455cd08d5e7e8be52
Thomas Schjerlund <thomas@u-man.dk>
2026-05-28 13:37:51 +0200

    Kan nu addCar og DeleteCar

--- CarApp.Core/obj/Debug/net10.0/CarApp.Core.AssemblyInfo.cs
+++ CarApp.Core/obj/Debug/net10.0/CarApp.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarApp.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99f9b33f56f71cb0cb9ba3673474157d3afa3b67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcad391717684c365bd1304781b27c4d1010b3f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarApp.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarApp.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CarApp.Wpf/ViewModels/CarViewModel.cs
+++ CarApp.Wpf/ViewModels/CarViewModel.cs
@@ -101,8 +101,28 @@ namespace CarApp.Wpf.ViewModels
         }
 
         // ── Disse to metoder får I i Øvelse 6 ──────────────
-        private void UpdateCar() { /* udleveres i Øvelse 6 */ }
-        private void DeleteCar() { /* udleveres i Øvelse 6 */ }
+        private void UpdateCar()
+        {
+            _repository.Update(SelectedCar);
+            RefreshCarList();
+        }
+
+        private void DeleteCar()
+        {
+            var result = MessageBox.Show(
+                $"Vil du slette {SelectedCar._brand} {SelectedCar._model}?",
+                "Bekræft sletning",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Warning);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                _repository.Delete(SelectedCar._licensePlate);
+                Cars.Remove(SelectedCar);
+                SelectedCar = new FuelCar("", "", DateTime.Now.Year, "", 0, 0, 0, false, FuelType.Benzin, 2000);
+            }
+        }
+
 
         private void RefreshCarList()
         {
